b7814ab443c0e1b381b3a9031be66c8a40a80b3d
DannyGonzalezO <dannygonzalez1327@hotmail.com>
2025-05-09 12:36:11 -0400

    fix: fix global position for bombs

--- scripts/bomb_placement_system.gd
+++ scripts/bomb_placement_system.gd
@@ -13,20 +13,35 @@ var explosion_size = 1
 func _ready() -> void:
 	player = get_parent()
 
-func place_bomb():
+@rpc("call_local")
+func place_bomb_rpc(pos: Vector2):
 	if bombs_placed == player.max_bombs_at_once:
 		return
 	
 	var bomb = BOMB_SCENE.instantiate()
-	var player_position = player.position
-	var bomb_position = Vector2(round(player_position.x / tile_size) * tile_size , \
-		round(player_position.y / tile_size) * tile_size)
-	print(bomb_position)
+	bomb.position = pos
 	bomb.explosion_size = explosion_size
-	bomb.position = bomb_position
 	get_tree().root.add_child(bomb)
 	bombs_placed += 1
 	bomb.tree_exiting.connect(on_bomb_exploded)
+
+func place_bomb():
+	if bombs_placed == player.max_bombs_at_once:
+		return
+	
+	var player_position = player.global_position
+	print(player.position)
+	var bomb_position = Vector2(
+		round(player_position.x / tile_size) * tile_size,
+		round(player_position.y / tile_size) * tile_size
+	)
+	print(bomb_position)
+	# Colocar local
+	place_bomb_rpc(bomb_position)
+
+	# Sincronizar con todos (incluye host también)
+	place_bomb_rpc.rpc(bomb_position)
+
 	
 func on_bomb_exploded():
 	bombs_placed -= 1
